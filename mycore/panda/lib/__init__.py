595906d052698c3ed92103e597ca03d6865d0df5
Anthony Theocharis <anthony@simplestation.com>
2010-09-08 19:34:58 -0700

    Fix bitrate setting in panda plugin

--- mycore/panda/lib/__init__.py
+++ mycore/panda/lib/__init__.py
@@ -491,7 +491,7 @@ class PandaHelper(object):
         new_media_file.size = d['file_size']
         ba = d.get('audio_bitrate', None) or 0
         bv = d.get('video_bitrate', None) or 0
-        new_media_file.bitrate = (ba + bv) or None
+        new_media_file.max_bitrate = (ba + bv) or None
         new_media_file.display_name = display_name
         return new_media_file
 
