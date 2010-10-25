12bf193a2af07b9b93644fccd025bdb46e6d4dd6
Nathan Wright <nathan@simplestation.com>
2010-10-25 14:02:03 -0700

    Fix unnecessary panda API calls on admin media edit

--- mycore/panda/__init__.py
+++ mycore/panda/__init__.py
@@ -19,20 +19,25 @@ def add_routes(mapper):
 
 @observes(events.Admin.MediaController.edit)
 def add_panda_vars(**result):
+    media = result['media']
     result['encoding_dicts'] = encoding_dicts = {}
     result['video_dicts'] = video_dicts = {}
     result['profile_names'] = {}
+
+    if not media.files:
+        return result
 
     storage = DBSession.query(PandaStorage).first()
     if not storage:
         return result
 
-    result['profile_names'] = storage.panda_helper.get_profile_ids_names()
-
-    for file in result['media'].files:
+    for file in media.files:
         encoding_dicts[file.id] = \
             storage.panda_helper.get_associated_encoding_dicts(file)
         video_dicts[file.id] = \
             storage.panda_helper.get_associated_video_dicts(file)
+
+    if video_dicts or encoding_dicts:
+        result['profile_names'] = storage.panda_helper.get_profile_ids_names()
 
     return result
